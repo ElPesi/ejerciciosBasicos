1d07776cf7e0918cd530f1015cd16a4203be8525
Tito Calderon <162237728+ElPesi@users.noreply.github.com>
2025-03-13 09:38:36 -0300

    Update ej7.py

--- ej7.py
+++ ej7.py
@@ -1,6 +1,27 @@
+def ingresoDatos():
+    
+    nombre = input("Ingresa tu nombre: ")
+    edad = int(input("Ingresa tu edad: "))
+    ciudad = input("Ingresa tu ciudad de residencia: ")
 
-nombre = input("Ingresa tu nombre: ")
-edad = int(input("Ingresa tu edad: "))
-ciudad = input("Ingrese tu ciudad: ")
-cumpleaños = 2025 - edad
-print = ("Nombre: " (nombre), "Edad: " (edad), "Ciudad: " (ciudad), "Cumpleaños: " (cumpleaños))
+    año_actual = 2025  
+    cumpleaños = año_actual - edad
+    print(f"Hola, {nombre}! Aquí está tu información:")
+    print(f"Nombre: {nombre}")
+    print(f"Edad: {edad}")
+    print(f"Ciudad: {ciudad}")
+    print(f"Año de nacimiento: {cumpleaños}")
+
+    if edad > 18:
+        print("Eres mayor de 18 años.")
+    else:
+        print("Eres menor de 18 años.")
+    
+    
+    if edad % 5 == 0:
+        print("Tu edad es un múltiplo de 5.")
+    else:
+        print("Tu edad no es un múltiplo de 5.")
+
+
+ingresoDatos()
